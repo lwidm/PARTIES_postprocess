2126f1e640fbd3680e077d4a3b73a31ebef95702
lwidm <lukas@widmer.id.au>
2025-10-07 16:40:21 -0700

    grouped stuff together in src

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@
 Lukas Widmer's (lwidm / lukwidmer / x-lwidmer) python package for postprocessing PARTIES simulations
 """
 
-from . import myio, theory, fluid, flocs, scripts, plotting, globals
+from .src import myio, theory, fluid, flocs, scripts, plotting, globals
 
 __version__ = "1.0.0"
 __author__ = "Lukas Widmer"
