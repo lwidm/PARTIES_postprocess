52201f86ced52372a061035006cc359930059626
lwidm <lukas@widmer.id.au>
2025-10-31 11:53:38 -0700

    updated double check

--- src/scripts/run_double_check.py
+++ src/scripts/run_double_check.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import numpy as np
 
 def main():
-    data_path: Path = Path("/anvil/scratch/x-lwidmer/RUN10/Data_166.h5")
+    data_path: Path = Path("/anvil/scratch/x-lwidmer/RUN10/Data_526.h5")
     output_data_path: Path = Path("output/RUN10/fluid/parties_reynolds.h5")
     u: np.ndarray
     with h5py.File(str(data_path), "r") as f:
