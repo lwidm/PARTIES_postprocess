b8a7287cb0961bec8570d589c2f6cae9ac5c6862
lwidm <lukas@widmer.id.au>
2025-11-04 10:31:38 -0800

    mistake

--- src/fluid/flow_statistics.py
+++ src/fluid/flow_statistics.py
@@ -479,7 +479,7 @@ def process_mean_phi_xz(
         with h5py.File(str(fluid_file), "r") as h5_file:
             dset = h5_file["vfw"]  # type: ignore
             dset.read_direct(vfw_buf, np.s_[:-1, :-1, :-1])
-        np.mean(vfw_buf, axis=(0, 1), out=vfw_mean_buf)
+        np.mean(vfw_buf, axis=(1, 2), out=vfw_mean_buf)
         results["Phi_mean"] += vfw_mean_buf
         phi_tot: float = np.sum(vfw_mean_buf, axis=None) / float(Nz)
         np.divide(vfw_mean_buf, phi_tot, out=vfw_mean_buf)
@@ -497,7 +497,7 @@ def process_mean_phi_xz(
             with h5py.File(str(fluid_file), "r") as h5_file:
                 dset = h5_file["vfw"]  # type: ignore
                 dset.read_direct(vfw_buf, np.s_[:-1, :-1, :-1])
-            np.mean(vfw_buf, axis=(0, 1), out=vfw_mean_buf)
+            np.mean(vfw_buf, axis=(1, 2), out=vfw_mean_buf)
             np.subtract(vfw_mean_buf, results["Phi_mean"], out=vfw_err_buf)
             np.multiply(vfw_err_buf, vfw_err_buf, out=vfw_err_buf)
             results["Phi_err"] += vfw_err_buf
